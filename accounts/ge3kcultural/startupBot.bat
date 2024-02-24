346324f8688d6eabafcfeb8a92b4686d2456be28
unknown <dragomir1520@gmail.com>
2024-02-24 21:06:09 +0200

    Adding error handling to startupBot.bat when Memu is not already started.

--- accounts/ge3kcultural/startupBot.bat
+++ accounts/ge3kcultural/startupBot.bat
@@ -2,14 +2,27 @@
 REM Wait for 15 seconds
 timeout /t 15
 
-REM Connect to the device using adb
-adb connect localhost:21503
+:connect
+REM Attempt to connect using adb
+adb connect localhost:21503 > tmp.txt 2>&1
 
-REM Change directory to your project folder and run the Python script
+REM Check if the connection was successful or if it needs to retry
+findstr /C:"cannot connect to 127.0.0.1:21503: No connection could be made because the target machine actively refused it. (10061)" tmp.txt
+if %errorlevel%==0 (
+    echo Connection failed, retrying...
+    goto connect
+) else (
+    echo Connection successful.
+)
+
+REM Proceed with the rest of the script
 cd /d C:\Users\dari\Desktop\projects\GrammAddict
 python run.py --config accounts/ge3kcultural/likeAndWatch.yml
 
-REM Prevent the script from closing
+REM Cleanup temporary file
+del tmp.txt
+
+REM Keep the window open
 :loop
 echo Script completed. Press CTRL+C to exit.
 timeout /t -1
